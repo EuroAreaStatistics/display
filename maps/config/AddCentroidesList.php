71ab9202f0c0bc73f7335dedc0e49cb716d72465
Alistair Cloete <cloete@adjoli.de>
2021-08-06 12:33:37 +0200

    convert all files in AddCentroidesList

--- maps/config/AddCentroidesList.php
+++ maps/config/AddCentroidesList.php
@@ -2,24 +2,40 @@
 
 // creates centroides list from centroides shapes
 
-ini_set ('display_errors','ON');
+ini_set ('display_errors', 'ON');
+ini_set ('serialize_precision', '14');
+header('Content-type', 'text/plain');
 
-//$path = 'countries/BWA';
-$path = 'cities';
+foreach ((array)glob(__DIR__.'/../json/*/*centroides.geojson') as $file) {
+  $input = json_decode(file_get_contents($file), TRUE);
+  $config = dirname($file) . '/featureReferenceCode.json';
+  $shapeCode = json_decode(file_get_contents($config), TRUE);
 
-$shapeCode = 'ISO_A3';
+  $output = [];
 
-$input = json_decode(file_get_contents("../json/$path/centroides.geojson"),TRUE);
+  foreach($input["features"] as &$feature) {
+    $output[$feature["properties"][$shapeCode]] = [
+      'Long' => $feature["geometry"]["coordinates"][0],
+      'Lat' => $feature["geometry"]["coordinates"][1],
+    ];
+  }
+  // add EU28 as a copy of EUN
+  if (array_key_exists("EUN", $output) && !array_key_exists("EU28", $output)) {
+    $output["EU28"] = $output["EUN"];
+  }
+  // add regions with dummy coordinate
+  foreach (['4A', '9A', 'R12'] as $code) {
+    if (!array_key_exists($code, $output)) {
+      $output[$code] = [
+        'Long' => 0,
+        'Lat' => 0,
+      ];
+    }
+  }
 
-$output = array();
-
-foreach($input["features"] as &$feature) {
-  $output[$feature["properties"][$shapeCode]] = array(
-    'Long' => $feature["geometry"]["coordinates"][0],
-    'Lat' => $feature["geometry"]["coordinates"][1]
-  );
-
+  $outfile = realpath(dirname($file) . '/' . basename($file, '.geojson') . '-list.json');
+  file_put_contents($outfile, json_encode($output));
+  echo "wrote $outfile\n";
 }
 
-file_put_contents("../json/$path/centroides-list.json", json_encode($output));
-echo "wrote centroides-list.json";
+echo "DONE\n";
