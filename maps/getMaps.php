3f307362a8a626f554c16e928713184863349c6f
Alistair Cloete <cloete@adjoli.de>
2021-08-06 17:17:59 +0200

    remove countries maps

--- maps/getMaps.php
+++ maps/getMaps.php
@@ -7,43 +7,22 @@ function getMaps ($country=null,$type='shapes',$themeURL='oecd') {
     $themeURL = 'oecd';
   }
 
-  $countries = array ('bwa','gmb','mwi','nga');
-  if (!in_array(strtolower($country),$countries)) {
-    $country = null;
-  }
-
-  if ($country == null) {
-    if ($type=='shapes') {
-      $map = __DIR__.'/json/'.$themeURL.'/world-shapes.geojson';
-    } elseif ($type=='shapesWithEU') {
-      $map = __DIR__.'/json/'.$themeURL.'/world-shapes1.geojson';
-    } elseif ($type=='centroides') {
-      $map = __DIR__.'/json/'.$themeURL.'/world-centroides.geojson';
-    } elseif ($type=='centroidesList') {
-      $map = __DIR__.'/json/'.$themeURL.'/world-centroides-list.json';
-    } elseif ($type=='centroidesListEU') {
-      $map = __DIR__.'/json/'.$themeURL.'/world-centroides-list1.json';
-    } elseif ($type=='disputed') {
-      $map = __DIR__.'/json/'.$themeURL.'/world-disputedLines.geojson';
-    } elseif ($type=='config') {
-      $map = __DIR__.'/json/'.$themeURL.'/featureReferenceCode.json';
-    } else {
-      $map = null;
-    }
+  if ($type=='shapes') {
+    $map = __DIR__.'/json/'.$themeURL.'/world-shapes.geojson';
+  } elseif ($type=='shapesWithEU') {
+    $map = __DIR__.'/json/'.$themeURL.'/world-shapes1.geojson';
+  } elseif ($type=='centroides') {
+    $map = __DIR__.'/json/'.$themeURL.'/world-centroides.geojson';
+  } elseif ($type=='centroidesList') {
+    $map = __DIR__.'/json/'.$themeURL.'/world-centroides-list.json';
+  } elseif ($type=='centroidesListEU') {
+    $map = __DIR__.'/json/'.$themeURL.'/world-centroides-list1.json';
+  } elseif ($type=='disputed') {
+    $map = __DIR__.'/json/'.$themeURL.'/world-disputedLines.geojson';
+  } elseif ($type=='config') {
+    $map = __DIR__.'/json/'.$themeURL.'/featureReferenceCode.json';
   } else {
-    if ($type=='shapes') {
-      $map = __DIR__.'/json/countries/'.strtoupper($country).'/shapes.geojson';
-    } elseif ($type=='centroides') {
-      $map = __DIR__.'/json/countries/'.strtoupper($country).'/centroides.geojson';
-    } elseif ($type=='centroidesList') {
-      $map = __DIR__.'/json/countries/'.strtoupper($country).'/centroides-list.json';
-    } elseif ($type=='disputed') {
-      $map = __DIR__.'/json/countries/'.strtoupper($country).'/disputedLines.geojson';
-    } elseif ($type=='config') {
-      $map = __DIR__.'/json/countries/'.strtoupper($country).'/featureReferenceCode.json';
-    } else {
-      $map = null;
-    }
+    $map = null;
   }
 
   return $map;
